9ab241d37fd2183a110fffc420008568bfb2c20d
Ranindu15 <ranindujayasooriya@gmail.com>
2020-03-10 12:17:04 +0530

    Update Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import NavigationBar from 'react-native-navbar';
 
 const Navbar = () => {
     return (
-        <View style={styles.container}>
-      <NavigationBar
-        title={titleConfig}
-        rightButton={rightButtonConfig}
-      />
+    <View style={styles.container}>
+        <NavigationBar title={titleConfig} rightButton={rightButtonConfig} />
     </View>
     )
 }
 const styles = {
     container: {
       flex: 1,
+      backgroundColor: '#ff9900',
     },
   };
   
   const rightButtonConfig = {
-    title: 'Next',
+    title: 'Sign-In',
     handler: () => alert('hello!'),
   };
   
   const titleConfig = {
-    title: 'Hello, world',
+    title: 'Hello Cars',
   };
+
 
   export default Navbar;
