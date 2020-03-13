346430f604048fb98075bfd68888e794cb70f137
Ranindu15 <ranindujayasooriya@gmail.com>
2020-03-13 13:01:15 +0530

    Update

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View } from 'react-native';
 import NavigationBar from 'react-native-navbar';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
 const Navbar = (navigation) => {
-  const Stack = createStackNavigator();
     return (
     <View style={styles.container}>
-        <NavigationBar style={styles.navbarstyl} title={titleConfig} rightButton={rightButtonConfig} />
-        <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('LoginForm')}
-      />
-      <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="login" component={LoginForm} />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <NavigationBar style={styles.navbarstyl} title={titleConfig} 
+        rightButton={rightButtonConfig} />
     </View>
     )
 }
@@ -31,7 +20,7 @@ const styles = {
   const rightButtonConfig = {
     title: 'Sign-In',
     tintColor: '#a29bfe',
-    handler: () => navigation.navigate('login'),
+    handler: () => alert('login'),
   };
   
   const titleConfig = {
